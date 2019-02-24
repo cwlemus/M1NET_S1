c72496d598d396bdcc6192ad8c0273b9b5e2e058
Carlos Enrique Lemus Serrano <kike_lemus@hotmail.com>
2019-02-23 20:47:41 -0600

    subida de proyecto

---
+++ M1NET_S1EJ1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1NET_S1EJ1
+{
+    public class Program
+    {
+        public string APublico = "Ambito publico";
+        private string APrivate = "Ambito privado";
+        protected internal string APotectedInternal = "Ambito con herencia y en todo el proyecto";
+        internal string AInternal = "Ambito interno";
+        protected string AProtected = "Ambito protected";
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
+
+namespace M1NET_S1EJ2
+{
+    using M1NET_S1EJ1;
+
+    class DemoAmbito2
+    {
+        //variables a las cuales podemos acceder
+        internal string VarInternal;
+        public string VarPublic;
+        public void PruebaAmbito()
+        {
+            Program prog = new Program();
+            VarInternal = prog.AInternal;
+            VarPublic = prog.APublico;
+            
+        }
+    }
+
+    class DemoAmbito1:Program
+    {
+        //variables a las cuales podemos acceder
+        internal string VarInternal;
+        public string VarPublic;
+        protected string VarProtected;
+        public void PruebaAmbito()
+        {
+            VarInternal = AInternal;
+            VarPublic = APublico;
+            VarProtected = AProtected;
+           
+        }
+    }
+}
+
+
+
---
+++ M1NET_S1EJ2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1NET_S1EJ2
+{
+    using  programAtributos= M1NET_S1EJ1.Program;
+    class Program
+    {
+        public  string VarPublico;
+        public void PruebaAmbito()
+        {
+           programAtributos p = new programAtributos();
+            VarPublico = p.APublico;
+        }
+    }
+
+    class DemoAmbito:programAtributos
+    {
+        public string VarPublico;
+        public string VarProtected;
+        public string VarProtectedInternal;
+        public void PruebaAmbito()
+        {
+            VarProtected = AProtected;
+            VarPublico = APublico;
+            VarProtectedInternal = APotectedInternal;
+        }
+    }
+}
